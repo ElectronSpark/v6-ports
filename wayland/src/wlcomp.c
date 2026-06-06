5c0078c26e9f9d048ae3cb3c9b94391a43a01dc4
electronspark <electronsparkle@gmail.com>
2026-06-06 15:37:27 -0400

    Update wlcomp for Linux fbdev ABI

--- wayland/src/wlcomp.c
+++ wayland/src/wlcomp.c
@@ -73,6 +73,7 @@ struct drm_virtgpu_getparam_compat {
 
 #define FBIOGET_VSCREENINFO  0x4600
 #define FBIOPUT_VSCREENINFO  0x4601
+#define FBIOGET_FSCREENINFO  0x4602
 #define FB_GPU_FILL_RECT     0x4610
 #define FB_GPU_BLIT          0x4611
 #define FB_GPU_BO_CREATE     0x4614
@@ -123,8 +124,60 @@ struct drm_virtgpu_getparam_compat {
 #define FB_GPU_CURSOR_MAX_DIM 64
 #define FB_GPU_CURSOR_F_VISIBLE 0x1
 
+struct fb_bitfield {
+    uint32_t offset;
+    uint32_t length;
+    uint32_t msb_right;
+};
+
 struct fb_var_screeninfo {
-    uint32_t xres, yres, bits_per_pixel, pitch;
+    uint32_t xres;
+    uint32_t yres;
+    uint32_t xres_virtual;
+    uint32_t yres_virtual;
+    uint32_t xoffset;
+    uint32_t yoffset;
+    uint32_t bits_per_pixel;
+    uint32_t grayscale;
+    struct fb_bitfield red;
+    struct fb_bitfield green;
+    struct fb_bitfield blue;
+    struct fb_bitfield transp;
+    uint32_t nonstd;
+    uint32_t activate;
+    uint32_t height;
+    uint32_t width;
+    uint32_t accel_flags;
+    uint32_t pixclock;
+    uint32_t left_margin;
+    uint32_t right_margin;
+    uint32_t upper_margin;
+    uint32_t lower_margin;
+    uint32_t hsync_len;
+    uint32_t vsync_len;
+    uint32_t sync;
+    uint32_t vmode;
+    uint32_t rotate;
+    uint32_t colorspace;
+    uint32_t reserved[4];
+};
+
+struct fb_fix_screeninfo {
+    char id[16];
+    uint64_t smem_start;
+    uint32_t smem_len;
+    uint32_t type;
+    uint32_t type_aux;
+    uint32_t visual;
+    uint16_t xpanstep;
+    uint16_t ypanstep;
+    uint16_t ywrapstep;
+    uint32_t line_length;
+    uint64_t mmio_start;
+    uint32_t mmio_len;
+    uint32_t accel;
+    uint16_t capabilities;
+    uint16_t reserved[2];
 };
 
 struct fb_gpu_fill {
