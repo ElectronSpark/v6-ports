9bcd5d93a657ce0a81c31befff1b62bbef3eaa1c
electronspark <electronsparkle@gmail.com>
2026-06-12 22:12:06 -0400

    xwayland: expose DRI3 through GLES glamor

---
+++ weston/xwayland-glamor-wrapper.c
@@ -0,0 +1,28 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int
+main(int argc, char **argv)
+{
+    const char *real = "/bin/Xwayland.real";
+    int extra = 2;
+    char **child = calloc((size_t)argc + (size_t)extra + 1, sizeof(*child));
+    if (!child) {
+        perror("calloc");
+        return 127;
+    }
+
+    child[0] = (char *)real;
+    child[1] = "-glamor";
+    child[2] = "es";
+    for (int i = 1; i < argc; i++)
+        child[i + extra] = argv[i];
+
+    execv(real, child);
+    fprintf(stderr, "Xwayland wrapper: execv(%s) failed: %s\n",
+            real, strerror(errno));
+    return 127;
+}
